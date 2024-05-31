817735f5eec92c12b7bd84ebe24437321352e70c
unknown <roman4uni@gmail.com>
2024-05-31 07:38:19 -0300

    posttp3

--- tp3/simpson.m
+++
@@ -1,5 +0,0 @@
-function w = simpson(h,fa,fb,fc)
-    w = h/3 *(fa+4*fb+fc);
-
-
- endfunction
--- tp3/trapcomp.m
+++
@@ -1,7 +0,0 @@
-function area = trapcomp(x,y)
-  n=length(x);
-  area = 0;
-  for i=1:n-1
-    area += trap(x(i+1)-x(i),y(i),y(i+1));
-  endfor
-  endfunction
